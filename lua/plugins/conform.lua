b053ae00e3f72cdf01117cbe31407199e864384e
Dimitar Todorov <zerefel@gmail.com>
2025-02-28 23:11:37 +0200

    feat: add sqlfluff formatter

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,11 +1,31 @@
 return {
-  {
-    "stevearc/conform.nvim",
-
-    opts = {
-      formatters_by_ft = {
-        sql = { "pg_format" },
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      sql = { "sqlfluff" },
+      pgsql = { "sqlfluff" },
+    },
+    formatters = {
+      sqlfluff = {
+        command = "sqlfluff",
+        args = { "format", "--dialect=postgres", "-" },
+        stdin = true,
       },
     },
   },
 }
+-- return {
+-- {
+--   "stevearc/conform.nvim",
+--   opts = {
+--     formatters_by_ft = {
+--       sql = { "pg_format" },
+--       pgsql = { "pg_format" },
+--     },
+--     formatters = {
+--       pg_format = {
+--         command = "pg_format",
+--       },
+--     },
+--   },
+-- },
