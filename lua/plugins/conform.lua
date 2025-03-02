1455a3f3ff37f8ec24323ec608124c68298bf3ae
Dimitar Todorov <zerefel@gmail.com>
2025-03-02 14:05:25 +0200

    feat: add sqlfluff formatting

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -10,22 +10,12 @@ return {
         command = "sqlfluff",
         args = { "format", "--dialect=postgres", "-" },
         stdin = true,
+        cwd = function()
+          return vim.fn.getcwd() -- Use current working directory
+          -- Or return a specific path:
+          -- return "/path/to/your/project"
+        end,
       },
     },
   },
 }
--- return {
--- {
---   "stevearc/conform.nvim",
---   opts = {
---     formatters_by_ft = {
---       sql = { "pg_format" },
---       pgsql = { "pg_format" },
---     },
---     formatters = {
---       pg_format = {
---         command = "pg_format",
---       },
---     },
---   },
--- },
